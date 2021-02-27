9127bd70c784f13b6f951a0ca1423cd26a2f803b
Triet Nguyen <nttriet9a4@gmail.com>
2021-02-27 14:23:21 +0700

    update medicine data

--- App/Thesis-IOS final/Thesis_2020/View/CameraView/CaptureImageView.swift
+++ App/Thesis-IOS final/Thesis_2020/View/CameraView/CaptureImageView.swift
@@ -8,28 +8,28 @@
 import SwiftUI
 
 struct CaptureImageView {
-  
-  /// MARK: - Properties
-  @Binding var isShown: Bool
-  @Binding var image: Image?
- @Binding var uiimage: UIImage?
-  
-  func makeCoordinator() -> Coordinator {
-    return Coordinator(isShown: $isShown, image: $image, uiimage: $uiimage)
-  }
+    
+    /// MARK: - Properties
+    @Binding var isShown: Bool
+    @Binding var image: Image?
+    @Binding var uiimage: UIImage?
+    
+    func makeCoordinator() -> Coordinator {
+        return Coordinator(isShown: $isShown, image: $image, uiimage: $uiimage)
+    }
 }
 
 extension CaptureImageView: UIViewControllerRepresentable {
-  func makeUIViewController(context: UIViewControllerRepresentableContext<CaptureImageView>) -> UIImagePickerController {
-    let picker = UIImagePickerController()
-    picker.sourceType = .camera
-    picker.delegate = context.coordinator
-    return picker
-  }
-  
-  func updateUIViewController(_ uiViewController: UIImagePickerController,
-                              context: UIViewControllerRepresentableContext<CaptureImageView>) {
+    func makeUIViewController(context: UIViewControllerRepresentableContext<CaptureImageView>) -> UIImagePickerController {
+        let picker = UIImagePickerController()
+        picker.sourceType = .camera
+        picker.delegate = context.coordinator
+        return picker
+    }
     
-    
-  }
+    func updateUIViewController(_ uiViewController: UIImagePickerController,
+                                context: UIViewControllerRepresentableContext<CaptureImageView>) {
+        
+        
+    }
 }
